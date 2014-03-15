175910a1bd488c55bd761259e1ec887eb6cf2ca0
TheD <TheD@Lenovo>
2014-03-15 14:20:56 +0100

    //pocetak fragmenta

--- Registrator/src/com/example/subota1/adapters/UserAdapter.java
+++ Registrator/src/com/example/subota1/adapters/UserAdapter.java
@@ -12,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class UserAdapter extends BaseAdapter
@@ -19,7 +20,7 @@ public class UserAdapter extends BaseAdapter
 
 	private Context mContext;
 	private ArrayList<UserModel> mUsers;
-
+	private ViewHolder mViewHolder;
 
 	public UserAdapter(Context context, ArrayList<UserModel> users) throws IllegalArgumentException {
 		if(context == null){
@@ -49,45 +50,55 @@ public class UserAdapter extends BaseAdapter
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		View view= LayoutInflater.from(mContext).inflate(R.layout.item_activity_second, parent, false);
-
-		TextView firstNameTextView = (TextView) view.findViewById(R.id.item_activity_second_firstName_textview);
-		TextView lastNameTextView = (TextView) view.findViewById(R.id.item_activity_second_lastName_textview);
-		TextView sexTextView = (TextView) view.findViewById(R.id.item_activity_second_sex_textview);
-		TextView phoneNumberTextView = (TextView) view.findViewById(R.id.item_activity_second_phoneNumber_textview);
-
-		UserModel korisnik = getItem(position);
-		if (korisnik != null){
+		if (convertView == null) {
+			mViewHolder=new ViewHolder();
 			
-			String tempFirstName = korisnik.getFirstName();
-			if( tempFirstName != null){
-				firstNameTextView.setText(tempFirstName);
-			}else {
-				firstNameTextView.setText("Prazno");
-			}
-			
-			String tempLastName = korisnik.getLastName();
-			if( tempLastName != null){
-				lastNameTextView.setText(tempLastName);
-			}else {
-				lastNameTextView.setText("Prazno");
-			}
-			
-			String tempSex = korisnik.getSex();
-			if( tempSex != null){
-				sexTextView.setText(tempSex);
-			}else {
-				sexTextView.setText("Prazno");
-			}
-			String tempPhoneNumber = korisnik.getPhoneNumber();
-			if( tempPhoneNumber != null){
-				phoneNumberTextView.setText(tempPhoneNumber);
-			}else {
-				phoneNumberTextView.setText("Prazno");
-			}
+			convertView=LayoutInflater.from(mContext).inflate(R.layout.item_activity_second, parent, false);
+			initViewHolderChildWidgets(convertView);
+			convertView.setTag(mViewHolder);
+		}else {
+			mViewHolder = (ViewHolder) convertView.getTag();
 		}
-		return view;
+		
+		UserModel user = getItem(position);
+		if (user != null) {
+			initViewHolderChildWidgets(user);
+		}
+		return convertView;
+	}
+	private void initViewHolderChildWidgets(UserModel user) {
+		String firstName = user.getFirstName();
+		if (firstName != null){
+		mViewHolder.firstNameTextView.setText(firstName);
+		}
+		String lastName = user.getLastName();
+		if (lastName != null){
+		mViewHolder.lastNameTextView.setText(lastName);
+		}
+		String sex = user.getSex();
+		if (sex != null){
+		mViewHolder.sexTextView.setText(sex);
+		}
+		String phoneNumber = user.getPhoneNumber();
+		if (phoneNumber != null){
+		mViewHolder.phoneNumberTextView.setText(phoneNumber);
+		}
 	}
 
+	private void initViewHolderChildWidgets(View convertView) {
+		mViewHolder.firstNameTextView = (TextView) convertView.findViewById(R.id.item_activity_second_firstName_textview);
+		mViewHolder.lastNameTextView = (TextView) convertView.findViewById(R.id.item_activity_second_lastName_textview);
+		mViewHolder.sexTextView = (TextView) convertView.findViewById(R.id.item_activity_second_sex_textview);
+		mViewHolder.phoneNumberTextView = (TextView) convertView.findViewById(R.id.item_activity_second_phoneNumber_textview);
+		mViewHolder.userImageView = (ImageView) convertView.findViewById(R.id.item_activity_second_imageView);
+		
+	}
+	public static class ViewHolder {
+		private TextView firstNameTextView;
+		private TextView lastNameTextView;
+		private TextView sexTextView;
+		private TextView phoneNumberTextView;
+		private ImageView userImageView;
+	}
 
 }
